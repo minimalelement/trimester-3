96e00b7fe8f93c977b651065bfcf1a2da348300a
Suraj K <95100047+minimalelement@users.noreply.github.com>
2022-03-24 13:52:06 +0530

    Add files via upload

---
+++ Java Programming/PLMS javaProject/plms.java
@@ -0,0 +1,50 @@
+class stadium
+{
+    String stadium_name, location,  pitch_size;
+    int phone;
+    int capacity;
+
+    void allstadiums()
+    {
+
+        //function to display stadiums in decreasing order of their capacity
+    
+    }
+}
+class team_details
+{
+    String teamname, logo_path; // array of objects for team
+
+    void teams()
+    {
+        // print all teams using array of objects
+    }
+
+    void search_team()
+    {
+        // search for a team in array
+    }
+}
+class player_info
+{
+    String playername, age, curnt_team;
+    int wages;
+    float height, weight;
+
+    void playerinfo()
+    {
+        // displays details of selected player
+    }
+}
+
+class transfers
+{
+    
+}
+
+class salary
+{
+    int 
+}
+}
+
